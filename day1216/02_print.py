0abc3f13269257817d235de9a06d118879432576
taehui <hth4236727@gmail.com>
2025-12-16 13:47:57 +0900

    파이썬 첫 print

---
+++ day1216/02_print.py
@@ -0,0 +1,54 @@
+print("Hello, Python!")
+print("함태희")
+print(100)
+print(3.14)
+print(True)
+
+print()
+
+print("나이: ", 20)
+print(1,2,3,4,5)
+print(1,2,3,4,5, sep=",")
+print(2025,12,15, sep="-")
+print("A","B","C", sep=" ")
+print("A","B","C", end=" ")
+
+print()
+print()
+
+name = "태희"
+print(name)
+a = 10
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+print()
+
+price = 12000
+print(f"가격은 {price}원입니다.")
+x = 7
+print(f"x가 {x}일때 x의 제곱은 {x**2}입니다." )
+
+# 학원 공지를 예쁘게 출력해보기
+title = "학원 공지"
+date = "2025-12-15"
+msg1 = "수업 시간: 18:00 ~ 20:00"
+msg2 = "준비물: 필기구, 노트"
+msg3 = "숙제: print() 실습 20문제"
+
+print("\n====================")
+print("　　　학원공지")
+print("====================")
+print("제목:", title)
+print("날짜:",date)
+print(msg1)
+print(msg2)
+print(msg3)
+
+'''
+여러줄로 설명을 넣고 싶을 때는
+이런식으로 따옴표 3개를 열고 닫는다.
+'''

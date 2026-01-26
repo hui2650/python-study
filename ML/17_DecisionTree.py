21a28dcdd0fcfd07e7fe3d81d6ed3b932da3fd28
taehui <hth4236727@gmail.com>
2026-01-26 18:08:29 +0900

    의사결정나무 - 특성별 중요도, 다중분류

--- ML/17_DecisionTree.py
+++ ML/17_DecisionTree.py
@@ -65,10 +65,10 @@ print('테스트 스코어:', dt.score(test_scaled, test_target))
 import matplotlib.pyplot as plt
 from sklearn.tree import plot_tree
 
-plt.figure(figsize=(10, 7))
-plot_tree(dt, max_depth=1, filled=True,
-          feature_names=['alcohol', 'sugar', 'pH']
-          )
+# plt.figure(figsize=(10, 7))
+# plot_tree(dt, max_depth=1, filled=True,
+#           feature_names=['alcohol', 'sugar', 'pH']
+#           )
 
 # 트리 깊이를 3으로 제한
 dt = DecisionTreeClassifier(max_depth=3, random_state=42)
@@ -82,4 +82,71 @@ plt.figure(figsize=(20, 15))
 plot_tree(dt, filled=True,
           feature_names=['alcohol', 'sugar', 'pH']
           )
+plt.show()
+
+# 트리 모델은 스케일링을 할 필요가 없다 / 스케일링의 영향을 전혀 받지 않는다
+dt = DecisionTreeClassifier(max_depth=3, random_state=42)
+dt.fit(train_input, train_target)
+
+print('\n========== 노 스케일링 깊이(3) 스코어 ==========\n')
+print('훈련 스코어:', dt.score(train_input, train_target))
+print('테스트 스코어:', dt.score(test_input, test_target))
+
+# 특성 별로 중요도를 뽑아보기 (합해서 1)
+# 불순도 감소에 누가 가장 많이 기여했는가?
+print('\n========== 특성별 중요도 ===========\n')
+print(dt.feature_importances_)
+
+# 정보 이득이 0.0005보다 적으면 더 이상 분할 하지 마라
+# 디폴트값 = 0
+dt = DecisionTreeClassifier(min_impurity_decrease=0.0005, random_state=42)
+dt.fit(train_input, train_target)
+
+'''디폴트 값은 사실상 제약이 없음 (과적합 머신)
+DecisionTreeClassifier(
+    max_depth=None,             # 무제한 
+    min_impurity_decrease=0.0,  # 아주 미세한 개선도 허용
+    min_samples_split=2,        # 샘플 2개만 있어도 분할 시도
+    min_samples_leaf=1)         # 리프에 1개 샘플 허용
+'''
+
+print('\n========== min_impurity_decrease=0.005 스코어 ==========\n')
+print('훈련 스코어:', dt.score(train_input, train_target))
+print('테스트 스코어:', dt.score(test_input, test_target))
+
+# plt.figure(figsize=(20, 15))
+# plot_tree(dt, filled=True, feature_names=['alcohol', 'sugar', 'pH'])
+# plt.show()
+
+
+# ================== 다중 분류 ==================
+
+fish = pd.read_csv('./data/fish_data.csv')
+print(fish.head())
+print()
+
+# 물고기 종류 확인 (7개)
+print(fish['Species'].unique())
+print()
+
+# 인풋 데이터
+fish_data = fish[['Weight', 'Length', 'Diagonal', 'Height', 'Width']]
+
+# 타겟 데이터
+fish_target = fish['Species']
+
+# 훈련/테스트 셋 분리
+fish_train_input, fish_test_input, fish_train_target, fish_test_target = train_test_split(fish_data, fish_target, random_state=42)
+
+# 모델 훈련
+dt = DecisionTreeClassifier(max_depth=50, random_state=42)
+dt.fit(fish_train_input, fish_train_target)
+
+# 모델 평가 (score)
+print('훈련 스코어:', dt.score(fish_train_input, fish_train_target))
+print('테스트 스코어', dt.score(fish_test_input, fish_test_target))
+
+# 트리 그래프
+plt.figure(figsize=(20, 15))
+plot_tree(dt, filled=True, feature_names=['Weight', 'Length', 'Diagonal', 'Height', 'Width'])
 plt.show()
